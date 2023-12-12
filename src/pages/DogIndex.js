dfc46d0e9ad1637b9848a9ff70da92e77ab96f59
AragonJ13 <jeanaragon0@gmail.com>
2023-12-12 10:45:12 -0800

    Dog Show halfway complete

--- src/pages/DogIndex.js
+++ src/pages/DogIndex.js
@@ -1,4 +1,37 @@
-const DogIndex = () => {
-    return <div>DogIndex</div>
-}
+import React from "react"
+import { Card, CardBody, CardTitle, CardSubtitle, Button } from "reactstrap"
+
+
+
+const DogIndex = ({ dogs }) => (
+    <main className="dog-index-cards">
+      {dogs?.map((dog, index) => (
+        <Card
+          style={{
+            width: "14rem"
+          }}
+          key={index}
+        >
+          <img alt={`profile of a dog named ${dog.name}`} src={dog.image} />
+          <CardBody>
+            <CardTitle tag="h5">{dog.name}</CardTitle>
+            <CardSubtitle className="mb-2 text-muted" tag="h6">
+              Age: {dog.age}
+            </CardSubtitle>
+            <Button>See More Details</Button>
+          </CardBody>
+        </Card>
+      ))}
+    </main>
+  );
+  
+
+
+
+
+    
+
+    
+
+
 export default DogIndex
--- src/pages/DogShow.js
+++ src/pages/DogShow.js
@@ -1,4 +1,15 @@
-const DogShow = () => {
-    return <div>DogShow</div>
+import React from "react"
+import { useParams } from "react-router-dom"
+
+const DogShow = ({ dogs }) => {
+    const { id } = useParams()
+    let currentDog = dog.find((dog) => dog.id === +id)
+
+    return (
+        <>
+        <h3>Dog Show Page</h3>
+        </>
+    )
 }
+
 export default DogShow
