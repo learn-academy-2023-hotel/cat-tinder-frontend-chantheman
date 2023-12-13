bfe1f03b9d1522087ca47dfce2b2a0709ac1e768
ChandlerGipson <chandlergipson96@gmail.com>
2023-12-12 16:17:31 -0800

    create function complete

--- src/App.js
+++ src/App.js
@@ -1,43 +1,41 @@
-import React, { useState } from 'react';
-import Footer from "./components/Footer"
-import Header from "./components/Header"
-import DogEdit from "./pages/DogEdit"
-import DogIndex from "./pages/DogIndex"
-import DogNew from "./pages/DogNew"
-import DogShow from "./pages/DogShow"
-import Home from "./pages/Home"
-import NotFound from "./pages/NotFound"
+    import React, { useState } from 'react';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import DogEdit from "./pages/DogEdit";
+import DogIndex from "./pages/DogIndex";
+import DogNew from "./pages/DogNew";
+import DogShow from "./pages/DogShow";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import { Routes, Route } from "react-router-dom";
+import mockDogs from "./mockDogs";
+
 import './App.css';
-import { Routes, Route } from "react-router-dom"
-import mockDogs from "./mockDogs"
-
-
 
 const App = () => {
-  const[dogs, setDogs] = useState(mockDogs)
-  
+  const [dogs, setDogs] = useState(mockDogs);
 
-  
+  const createDog = (dog) => {
+    console.log("created dog:", dog);
+  };
+
   return (
     <>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/dogindex" element={<DogIndex dogs={dogs} />} />
-        <Route path="/dogshow/:id" element={<DogShow dogs={dogs} />} />
-        <Route path="/dognew" element={<DogNew />} />
         <Route path="/dogedit" element={<DogEdit />} />
+        <Route path="/dogindex" element={<DogIndex dogs={dogs} />} />
+        <Route path="/dognew" element={<DogNew createDog={createDog} />} />
+        <Route path="/dogshow/:id" element={<DogShow dogs={dogs} />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
     </>
-  )
-}
-       
-        
-export default App;        
-        
-      
+  );
+};
+
+export default App;
 
 
  
